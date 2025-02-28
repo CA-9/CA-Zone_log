82c307e0c053a2fe87e2d707d197955df2da826c
CA.1 <mohammad92007@gmail.com>
2025-02-28 22:29:27 +0400

    config.lua

---
+++ config.lua
@@ -0,0 +1,16 @@
+Cadev = {}
+
+Cadev.CoreName = 'qb-core'
+
+Cadev.Zones = {
+    {
+        name = "Under_ground.1",
+        coords = vector3(-147.21, 6435.39, 31.92),
+        radius = 2.7,
+        useZ = true,
+        debugPoly = true,
+        requiredItems = {
+            { name = "weapon_crowbar", count = 1 }
+        }
+    },
+}
